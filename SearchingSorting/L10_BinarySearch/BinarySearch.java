fd2f18efccafb05b7c770e0896cacce86d7c66f5
969hadiahmed@gmail.com <969hadiahmed@gmail.com>
2023-07-09 22:53:57 +0530

    L10 Binary Search

---
+++ SearchingSorting/L10_BinarySearch/BinarySearch.java
@@ -0,0 +1,37 @@
+package SearchingSorting.L10_BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arrA[]={0,2,5,7,8,9,14,17,19,25,27,32,38,41,46};
+        System.out.println(binarySearchAscending(arrA, 7));
+        int arrD[]={29,27,24,21,18,16,13,9,7,5,3,1,0};
+        System.out.println(binarySearchDecending(arrD, 29));
+        System.out.println(binarySearchDecending(arrD, 21));
+        System.out.println(binarySearchDecending(arrD, 16));
+        System.out.println(binarySearchDecending(arrD, 9));
+        System.out.println(binarySearchDecending(arrD, 3));
+        System.out.println(binarySearchDecending(arrD, 0));
+        System.out.println(binarySearchDecending(arrD, 45));
+
+    }
+    static int binarySearchAscending(int[] arr,int target){
+        int l=0,h=arr.length-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(arr[mid]>target) h=mid-1;
+            else if(arr[mid]<target)l=mid+1;
+            else return mid;
+        }
+        return -1;
+    }
+    static int binarySearchDecending(int[] arr,int target){
+        int l=0,h=arr.length-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(arr[mid]<target) h=mid-1;
+            else if(arr[mid]>target)l=mid+1;
+            else return mid;
+        }
+        return -1;
+    }
+}

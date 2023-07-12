d0fe6b8e38e5392d8bd517a3afe20faee9ccfd47
969hadiahmed@gmail.com <969hadiahmed@gmail.com>
2023-07-12 18:08:13 +0530

    Leet Code 744

--- SearchingSorting/L10_BinarySearch/L11_CeilFloor.java
+++ SearchingSorting/L10_BinarySearch/L11_CeilFloor.java
@@ -1,6 +1,6 @@
 package SearchingSorting.L10_BinarySearch;
 
-public class CeilFloor {
+public class L11_CeilFloor {
     public static void main(String[] args) {
         int[] arr={0,2,5,7,9,13,17,19,21,25,28,32,35,37,39,43,48,49,53,57,64,69,74,85,89,93,96};
         //        {0 1 2 3 4 5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26}
